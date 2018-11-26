c7c63e31e5b5bb6f5b7c8f9db22db85a01d778a1
Tracey L. Marsh <tmarsh@fredhutch.org>
2018-11-26 14:13:15 -0800

    Dreams

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -19,4 +19,8 @@ hello <- function() {
 
 goodbye <- function(){
   print("Goodbye, cruel world.")
+}
+
+sweetDreams <- function(){
+  print("Sweet dreams. Don't let the bed bugs bite!")
 }
